5250e4bd0e4b2cff5cdc142008a58c86dc9933b8
catalinbroinas <catalinbroinas@yahoo.com>
2025-02-22 21:51:08 +0200

    feat: add navbar-scroll effect on page scroll

--- src/js/main.js
+++ src/js/main.js
@@ -3,8 +3,18 @@ import '../scss/main.scss';
 import { Ripple, Dropdown, Collapse, Input, Tooltip, initMDB } from 'mdb-ui-kit/js/mdb.es.min.js';
 
 function MainDomManager() {
+    const navbarScroll = () => {
+        const navbar = document.querySelector('#navbar');
+
+        window.addEventListener('scroll', () => {
+            (window.pageYOffset > 100) ? navbar.classList.add('navbar-scroll')
+                : navbar.classList.remove('navbar-scroll');
+        });
+    };
+
     const initApp = () => {
         initMDB({ Ripple, Dropdown, Collapse, Input, Tooltip });
+        navbarScroll();
     }
 
     return { initApp };
